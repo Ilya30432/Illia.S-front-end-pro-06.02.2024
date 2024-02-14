2653f455fa1d1d516c6add59663f5ba8ba0b59d6
Ilya30432 <IlyaS30432@gmail.com>
2024-02-14 22:57:59 +0100

    lesson 3

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 console.log('Hello world');  
 console.log('witam'); //консоль
+
+
+let bthText = document.querySelector("#buto");
+
+bthText.onclick = function () {
+    let text = prompt('Напишите 3 слова');
+    alert (`${text}`);
+};
+
+let bthNum = document.querySelector("#butos");
+
+bthNum.onclick = function () {
+    let texts = prompt ('Напишите только 5 цифер');
+    let number = texts.split('');
+    alert(`${number}`);
+};
